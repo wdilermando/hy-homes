140e05c5a555a0b9c9eb169f757ca6e17e439116
w.cosmo.da.silva <w.cosmo.da.silva@avanade.com>
2022-01-26 18:02:43 -0300

    text

--- src/components/cards/CardListing.js
+++ src/components/cards/CardListing.js
@@ -46,8 +46,8 @@ export default function CardListing() {
           </p>
           <p className="text-gray-400 text-sm mb-4 line-clamp-3">
             Some quick example text to build on the card title and make up the
-            bulk of the card's content. Some quick example text to build on the
-            card title and make up the bulk of the card's content.
+            bulk of the cards content. Some quick example text to build on the
+            card title and make up the bulk of the cards content.
           </p>
         </div>
         <div className="px-4 pb-4 flex items-center justify-start text-gray-600 text-sm">
